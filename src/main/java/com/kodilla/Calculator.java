932c8cbd9f9e1d38d52ffb60e60c7941e3d37d58
tomysnatalia <hanf.natalia@gmail.com>
2019-12-04 13:15:21 +0100

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+public class Calculator {
+
+
+    public static void someMethod() {
+        System.out.println("It works!");
+    }
+
+    public static void main (String [] args) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+        }
+    }
+
