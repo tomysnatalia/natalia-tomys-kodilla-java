4815008ba9895a596a9f3912c4a0493093afdb60
tomysnatalia <hanf.natalia@gmail.com>
2020-01-23 19:50:53 +0100

    First Commit

--- kodilla-rps/src/test/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/test/java/com/kodilla/rps/RpsRunner.java
@@ -12,8 +12,8 @@ public class RpsRunner {
         int player;
         int computer;
         int playerPoints = 0;
-        int computerPoints=0;
-
+        int computerPoints = 0;
+        boolean end = false;
 
 
         System.out.println(" **** Hello let's start to play in Rock, Paper, Scissors ****\n");
@@ -26,77 +26,61 @@ public class RpsRunner {
         String userName = scan.nextLine();
         System.out.println("*** Hello " + userName + " *** to how many wins you want to play? ");
         int counter = scan.nextInt();
-        System.out.println("Okey up to "+ counter + " wins");
+        System.out.println("Okey up to " + counter + " wins");
+        System.out.println("Start!! your move");
 
 
         Random random = new Random();
-        player = scan.nextInt();
-        computer = random.nextInt(3)+1;
 
 
-        while(playerPoints < counter ) {
+        while (playerPoints < counter || computerPoints < counter) {
             player = scan.nextInt();
             computer = random.nextInt(3);
-            counter++;
-            if (playerPoints == counter) {
-                System.out.println(" Congratulations " + userName + " WON !!!!!");
+            if (player == 1 && computer == 3) {
+                System.out.println(userName + " choose Rock ");
+                System.out.println("Computer choose Scissors\n" + userName + " won!!");
+                playerPoints++;
+
+            } else if (player == 1 && computer == 2) {
+                System.out.println(userName + " choose Rock ");
+                System.out.println("Computer choose Paper\n Computer Won!!");
+                computerPoints++;
+
+            } else if (player == 1 && computer == 1) {
+                System.out.println(userName + " choose Rock ");
+                System.out.println("Computer choose Rock \n Game Tie!!");
+
+            } else if (player == 2 && computer == 2) {
+                System.out.println(userName + " choose Paper");
+                System.out.println("Computer choose Paper \n Game Tie!!");
+
+            } else if (player == 2 && computer == 1) {
+                System.out.println(userName + " choose Paper");
+                System.out.println("Computer choose Rock \n" + userName + " won!!");
+                playerPoints++;
+
+            } else if (player == 2 && computer == 3) {
+                System.out.println(userName + " choose Paper");
+                System.out.println("Computer choose Scissors \n Computer won!!");
+                computerPoints++;
+
+            } else if (player == 3 && computer == 3) {
+                System.out.println(userName + " choose Scissors");
+                System.out.println("Computer choose Scissors \n Game tie!!");
+
+            } else if (player == 3 && computer == 2) {
+                System.out.println(userName + " choose Scissors");
+                System.out.println("Computer choose Paper\n" + userName + " won!!");
+                playerPoints++;
+
+            } else if (player == 3 && computer == 1) {
+                System.out.println(userName + " choose Scissors");
+                System.out.println("Computer choose Rock \n Computer won!!");
+                computerPoints++;
+
+            } else {
+                System.out.println("Something goes wrong \n try again");
             }
-        }
-
-        while(computerPoints < counter) {
-            player = scan.nextInt();
-            computer = random.nextInt(3);
-            counter++;
-            if (computerPoints == counter) {
-                System.out.println("Computer won ! - TRY AGAIN ");
-            }
-        }
-
-
-        if (player == 1 && computer == 3) {
-            System.out.println(userName + " choose Rock ");
-            System.out.println("Computer choose Scissors\n" + userName + " won!!");
-            playerPoints++;
-
-        } else if (player == 1 && computer == 2) {
-            System.out.println(userName + " choose Rock ");
-            System.out.println("Computer choose Paper\n Computer Won!!");
-            computerPoints++;
-
-        } else if (player == 1 && computer == 1) {
-            System.out.println( userName + " choose Rock ");
-            System.out.println("Computer choose Rock \n Game Tie!!");
-
-        } else if (player == 2 && computer == 2) {
-            System.out.println(userName + " choose Paper");
-            System.out.println("Computer choose Paper \n Game Tie!!");
-
-        } else if (player == 2 && computer == 1) {
-            System.out.println(userName + " choose Paper");
-            System.out.println("Computer choose Rock \n" + userName + " won!!");
-            playerPoints++;
-
-        } else if (player == 2 && computer == 3) {
-            System.out.println(userName + " choose Paper");
-            System.out.println("Computer choose Scissors \n Computer won!!");
-            computerPoints++;
-
-        } else if (player == 3 && computer == 3) {
-            System.out.println(userName + " choose Scissors");
-            System.out.println("Computer choose Scissors \n Game tie!!");
-            counter++;
-        } else if (player == 3 && computer == 2) {
-            System.out.println(userName + " choose Scissors");
-            System.out.println("Computer choose Paper\n" + userName + " won!!");
-            playerPoints++;
-
-        } else if (player == 3 && computer == 1) {
-            System.out.println(userName + " choose Scissors");
-            System.out.println("Computer choose Rock \n Computer won!!");
-            computerPoints++;
-
-        } else {
-            System.out.println("Something goes wrong \n try again");
         }
     }
 }
