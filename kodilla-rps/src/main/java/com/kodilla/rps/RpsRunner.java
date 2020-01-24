7d6a1f5f39a53dce534c2fe06194b034deded425
tomysnatalia <hanf.natalia@gmail.com>
2020-01-24 19:01:22 +0100

    First Commit

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -13,27 +13,23 @@ public class RpsRunner {
         int computer;
         int playerPoints = 0;
         int computerPoints = 0;
-        boolean end = false;
 
 
         System.out.println(" **** Hello let's start to play in Rock, Paper, Scissors ****");
         System.out.println("ROCK = 0, PAPER = 1, SCISSORS = 2");
         System.out.println();
 
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter your name: ");
         String userName = scan.nextLine();
-        System.out.println("*** Hello " + userName + " ***\nTo how many wins you want to play? ");
+        System.out.println("      *** Hello " + userName + " ***\nTo how many wins you want to play? ");
         int counter = scan.nextInt();
         System.out.println("Okey up to " + counter + " wins");
         System.out.println("Start!! It's your move");
 
-
         Random random = new Random();
 
-
-        while (playerPoints < counter || computerPoints < counter) {
+        while (playerPoints < counter ||computerPoints < counter) {
             player = scan.nextInt();
             computer = random.nextInt(2);
             if (player == 0 && computer == 2) {
@@ -83,9 +79,14 @@ public class RpsRunner {
             if (computerPoints == counter) {
                 System.out.println("Computer won!! - TRY AGAIN");
                 System.out.println("Computer points: " + computerPoints + " Player points: " + playerPoints);
+                PlayAgain playAgain = new PlayAgain();
+                playAgain.playAgain();
+
             } else if (playerPoints == counter) {
                 System.out.println("CONGRATULATIONS **** " + userName + " **** YOU ARE THE WINNER!");
                 System.out.println("Computer points: " + computerPoints + " Player points: " + playerPoints);
+                PlayAgain playAgain = new PlayAgain();
+                playAgain.playAgain();
             }
         }
     }
