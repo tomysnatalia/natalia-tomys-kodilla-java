9fb9574014ee18ba15f7a132b925cedf9d25a7bd
tomysnatalia <hanf.natalia@gmail.com>
2020-01-24 16:55:40 +0100

    First Commit

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -16,15 +16,15 @@ public class RpsRunner {
         boolean end = false;
 
 
-        System.out.println(" **** Hello let's start to play in Rock, Paper, Scissors ****\n");
-        System.out.println("ROCK = 1, PAPER = 2, SCISSORS = 3");
+        System.out.println(" **** Hello let's start to play in Rock, Paper, Scissors ****");
+        System.out.println("ROCK = 0, PAPER = 1, SCISSORS = 2");
         System.out.println();
 
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter your name: ");
         String userName = scan.nextLine();
-        System.out.println("*** Hello " + userName + " ***\n to how many wins you want to play? ");
+        System.out.println("*** Hello " + userName + " ***\nTo how many wins you want to play? ");
         int counter = scan.nextInt();
         System.out.println("Okey up to " + counter + " wins");
         System.out.println("Start!! It's your move");
@@ -82,12 +82,12 @@ public class RpsRunner {
 
             if (computerPoints == counter) {
                 System.out.println("Computer won!! - TRY AGAIN");
+                System.out.println("Computer points: " + computerPoints + " Player points: " + playerPoints);
             } else if (playerPoints == counter) {
-                System.out.println("CONGRATULATIONS ****" + userName + " **** YOU ARE THE WINNER!");
+                System.out.println("CONGRATULATIONS **** " + userName + " **** YOU ARE THE WINNER!");
+                System.out.println("Computer points: " + computerPoints + " Player points: " + playerPoints);
             }
-            System.out.println("Computer points: " + computerPoints + " Player points: " + playerPoints);
         }
-
     }
 }
 
