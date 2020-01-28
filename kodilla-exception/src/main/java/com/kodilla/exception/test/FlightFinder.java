dc82676e188a7f71d1100d6de02aed238bc9da1f
tomysnatalia <hanf.natalia@gmail.com>
2020-01-28 16:38:54 +0100

    First Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -7,24 +7,28 @@ public class FlightFinder {
     private Map<String, Boolean> airports;
 
 
-    private boolean findAirport (String arrivalAirport) throws RouteNotFoundException{
-        if (airports.containsKey(arrivalAirport)) {
-            return airports.get(arrivalAirport);
-        }else{
-            throw new RouteNotFoundException("Airport does not exist!");
-        }
+    public FlightFinder() {
+        airports = airports();
     }
-
 
     private Map<String, Boolean> airports() {
         Map<String, Boolean> airports = new HashMap<>();
-        airports.put("KRK",false );
+        airports.put("WAW", true);
         airports.put("ROMA", true);
         airports.put("LONDON", true);
         airports.put("PARIS", true);
 
         return airports;
+    }
 
+
+    boolean findAirport(Flight arrivalAirport) throws RouteNotFoundException {
+        if (airports.containsKey(arrivalAirport)) {
+            return airports.get(arrivalAirport);
+        } else {
+            throw new RouteNotFoundException("Airport does not exist!");
+        }
     }
 }
+
 
